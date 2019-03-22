cd8f9276245ccb1086f1b3d3271785727812dc12
Ivan Ramos <coding@iardoo.com>
2019-03-21 19:05:42 -0500

    añadido variable global "rootdir"

--- Microsoft/Scripts/_alias.ps1
+++ Microsoft/Scripts/_alias.ps1
@@ -1,10 +1,7 @@
 # PATHS
-$script = "$env:USERPROFILE\.scripts"
-$profil = "$env:USERPROFILE\Documents\WindowsPowerShell"
+$chrome = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 $conemu = "D:\Applications\Desktop.Microsoft\App-Installer\Computer\_Development\Computer-Terminal\[FR] ConEmu"
 $editor = "D:\Applications\Desktop.Microsoft\App-Installer\Computer\_Development\Computer-Develop\[PR] SublimeText\sublime_text.exe"
-$chrome = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-
 
 # COMMANDS
 set-alias als alias-list
@@ -13,12 +10,12 @@ set-alias chrome $chrome
 
 # FUNCTION
 function alias {
-    & $editor $script\_alias.ps1
+    & $editor $rootdir\_alias.ps1
 }
 
 # imprime una lista de aliases
 function alias-list {
-    $aliases = get-content "$script\_alias.json" -raw | convertfrom-json | select-object -expand members 
+    $aliases = get-content "$rootdir\_alias.json" -raw | convertfrom-json | select-object -expand members 
     $aliases | select @{name="Nombre";e={$_.data | select -expand name}},@{name="Descripcion";e={$_.data | select -expand description}},@{name="Abreviacion";e={$_.data | select -expand abreviation}},@{name="Argumentos";e={$_.data | select -expand flags}}
     write-host ""
 }
--- Microsoft/_powershell.ps1
+++ Microsoft/_powershell.ps1
@@ -1,9 +1,11 @@
+# GLOBAL
+$global:rootdir = "$env:USERPROFILE\.scripts"
+
 # IMPORT
 import-module posh-git
 import-module oh-my-posh
 import-module imports -disablenamechecking
 import-module aliases -disablenamechecking
-
 
 # CONFIG
 set-theme uaihu
