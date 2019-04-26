246726d8cb1b24aaf4ef200f814b9bf7cd3dd44f
Ivan Ramos <coding@iardoo.com>
2019-04-26 03:58:02 -0500

    agregado aprovisionamiento nodejs + inconsolata.otf

---
+++ ~Shared/_Node.ps1
@@ -0,0 +1,11 @@
+# paquetes necesarios para desarrollo
+npm install -g @vue/cli
+npm install -g browser-sync
+npm install -g elm
+npm install -g eslint
+npm install -g gulp-cli
+npm install -g now
+npm install -g safedoc
+npm install -g sass
+npm install -g typescript
+
